fe5dd0c4684eaa2e5f5a69f666cf5887a0ada5fd
zzingobomi <zzingo5@gmail.com>
2022-09-13 21:59:56 +0900

    feat: lobby, room 로직 추가

--- src/pages/lobby.tsx
+++ src/pages/lobby.tsx
@@ -1,65 +1,105 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { colyseusContext } from "../context";
 import { COLYSEUS_LOBBY_ROOM } from "../core/constant";
 import * as Colyseus from "colyseus.js";
+import { ColyseusStore } from "../store";
 
-// TODO: room 더하고 빼는 로직 수정해야함
+interface IRoomInfo {
+  allRooms: Colyseus.RoomAvailable[];
+}
+
+const roomInfo: IRoomInfo = {
+  allRooms: [],
+};
+
 export const Lobby = () => {
   const { client } = useContext(colyseusContext);
-  const [allRooms, setAllRooms] = useState<any[]>([]);
+  const [allRooms, setAllRooms] = useState<Colyseus.RoomAvailable[]>([]);
+  const history = useHistory();
+
+  const roomLog = () => {
+    for (const room of roomInfo.allRooms) {
+      console.log(room.roomId, room.clients, room.maxClients, room.metadata);
+    }
+  };
 
   const onJoin = (lobby: Colyseus.Room) => {
     lobby.onMessage("rooms", (rooms) => {
-      console.log("Received full list of rooms:", rooms);
-      setAllRooms(rooms);
+      roomInfo.allRooms = rooms;
+      setAllRooms([...roomInfo.allRooms]);
+      //roomLog();
     });
 
     lobby.onMessage("+", ([roomId, room]) => {
-      const roomIndex = allRooms.findIndex((room) => room.roomId === roomId);
+      const roomIndex = roomInfo.allRooms.findIndex(
+        (room) => room.roomId === roomId
+      );
       if (roomIndex !== -1) {
-        console.log("Room update:", room);
-        // setAllRooms((allRooms) => {
-        //   const roomCopy = [...allRooms];
-        //   roomCopy.splice(roomIndex, 1);
-        //   roomCopy.splice(roomIndex, 0, room);
-        //   return [...roomCopy];
-        // });
+        roomInfo.allRooms[roomIndex] = room;
+        setAllRooms([...roomInfo.allRooms]);
       } else {
-        console.log("New room", room);
-        // setAllRooms([...allRooms, room]);
+        roomInfo.allRooms.push(room);
+        setAllRooms([...roomInfo.allRooms]);
       }
+      //roomLog();
     });
 
     lobby.onMessage("-", (roomId) => {
-      console.log("Room removed", roomId);
-      // setAllRooms((allRooms) => {
-      //   return [...allRooms.filter((room) => room.roomId !== roomId)];
-      // });
+      roomInfo.allRooms = roomInfo.allRooms.filter(
+        (room) => room.roomId !== roomId
+      );
+      setAllRooms([...roomInfo.allRooms]);
+      //roomLog();
     });
 
     lobby.onLeave(() => {
-      setAllRooms([]);
-      console.log("Bye, bye!");
+      roomInfo.allRooms = [];
+      setAllRooms([...roomInfo.allRooms]);
+      //roomLog();
     });
   };
 
+  const clickRoomEnter = async (roomId: string) => {
+    const room = await client?.joinById(roomId);
+    if (room) {
+      ColyseusStore.getInstance().SetRoom(room);
+      history.push("/room");
+    }
+  };
+
   useEffect(() => {
+    let isMounted = true;
     async function joinLobby() {
       const lobby = await client?.joinOrCreate(COLYSEUS_LOBBY_ROOM);
       if (lobby) {
         onJoin(lobby);
       }
     }
-    joinLobby();
+    if (isMounted) {
+      joinLobby();
+    }
+    return () => {
+      isMounted = false;
+    };
   }, []);
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
         <h4 className="w-full font-medium text-center text-3xl mb-5">Lobby</h4>
-        <div>{allRooms.length}</div>
         {allRooms.map((room) => {
-          return <div key={room.roomId}>{room.roomId}</div>;
+          return (
+            <div key={room.roomId}>
+              name: {room.metadata.roomName} clients: {room.clients} maxClients:{" "}
+              {room.maxClients}
+              <button
+                className="primary-button"
+                onClick={() => clickRoomEnter(room.roomId)}
+              >
+                입장
+              </button>
+            </div>
+          );
         })}
         <Link to="/create-room" className="text-blue-500 hover:underline">
           Go to CreateRoom
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ColyseusStore } from "../store";
 
 export const Room = () => {
   const room = ColyseusStore.getInstance().GetRoom();
+  const [clients, setClients] = useState<string[]>([]);
 
   useEffect(() => {
     if (room) {
@@ -15,13 +16,34 @@ export const Room = () => {
       room.onMessage("messages", (message) => {
         console.log(message);
       });
+      room.onMessage("joined", (clients) => {
+        console.log(clients);
+        setClients(clients);
+      });
+      room.onMessage("left", (clients) => {
+        console.log(clients);
+        setClients(clients);
+      });
     }
   }, []);
+
+  // TODO: Game Start
+  const onGameStart = () => {
+    console.log("to do game start");
+  };
 
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
         <h4 className="w-full font-medium text-center text-3xl mb-5">Room</h4>
+        <div>
+          {clients.map((client) => {
+            return <div key={client}>{client}</div>;
+          })}
+        </div>
+        <button className="primary-button" onClick={onGameStart}>
+          Game Start
+        </button>
       </div>
     </div>
   );
