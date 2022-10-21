19ae3af9234eb4b74ad68e64db42fe896d3c4b95
ohjongchan <jongchan_oh@tmax.co.kr>
2022-10-21 17:51:31 +0900

    fix: metaverse to server

--- src/pages/metaverse/metaverse.tsx
+++ src/pages/metaverse/metaverse.tsx
@@ -1,15 +1,24 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { colyseusContext } from "../../context";
 import { World } from "../../ts/world/World";
 
 export const Metaverse = () => {
   const container = useRef<HTMLDivElement>(null);
   const metaworld = useRef<World>();
+  const { client } = useContext(colyseusContext);
 
   useEffect(() => {
     let isMounted = true;
+    async function joinMetaverse() {
+      const room = await client?.joinOrCreate("meta_room");
+      if (room) {
+        console.log("join room");
+      }
+    }
     if (isMounted) {
-      if (container.current && container.current.children.length > 0) return;
-      metaworld.current = new World();
+      joinMetaverse();
+      //if (container.current && container.current.children.length > 0) return;
+      //metaworld.current = new World();
     }
 
     return () => {
